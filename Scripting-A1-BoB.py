74bc4a75931587c095ea2e731f5245a3c8d78dce
Shadowdrums <122622018+Shadowdrums@users.noreply.github.com>
2023-03-01 03:23:39 -0500

    Add files via upload

--- Scripting-A1-BoB.py
+++ Scripting-A1-BoB.py
@@ -1,63 +1,56 @@
 import os
-import time
 import random
+import time
+import glob
+import sqlite3
 
-# Set the maximum time limit for writing a script
-MAX_TIME = 120
+dir_path = os.path.dirname(os.path.realpath(__file__))
+output_dir = os.path.join(dir_path, 'AI-Scripting-Tests')
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-# Create a new subfolder for saving the output files
-if not os.path.exists("AI-Scripting-Tests"):
-    os.makedirs("AI-Scripting-Tests")
+db_conn = sqlite3.connect("BoB-Database")
+db_cursor = db_conn.cursor()
+db_cursor.execute('''CREATE TABLE IF NOT EXISTS scripts
+                (id INTEGER PRIMARY KEY AUTOINCREMENT, script TEXT)''')
+db_conn.commit()
 
-# Initialize a counter for the sandbox files
-sandbox_num = 1
+MAX_SCRIPT_SIZE = 1000
 
-# Start an infinite loop to keep the program running
+def generate_script():
+    scripts = [f for f in glob.glob(os.path.join(dir_path, '**/*.py'), recursive=True) if not f.endswith('sandbox.py')]
+    script = ''
+    for f in scripts:
+        with open(f, 'r') as file:
+            script += file.read()
+
+    generated_script = ''
+    start_time = time.time()
+    while len(generated_script.split('\n')) < MAX_SCRIPT_SIZE and time.time() - start_time < 60:
+        generated_script += random.choice(script.split('\n')) + '\n'
+
+    return generated_script
+
 while True:
+    goal = input("Enter the goal of the script: ")
+    input(f"Press Enter to generate a new script with the goal: {goal}")
+    generated_script = generate_script()
+    print(f"Generated script:\n{generated_script}")
 
-    # Generate a new sandbox file name
-    sandbox_file = f"sandbox-{sandbox_num}.py"
-    
-    # Check if the file already exists (in case of a previous run)
-    while os.path.exists(sandbox_file):
-        sandbox_num += 1
-        sandbox_file = f"sandbox-{sandbox_num}.py"
-    
-    # Open the sandbox file for writing
-    with open(sandbox_file, "w") as f:
-        
-        # Get a list of all Python scripts in the current directory and its subfolders
-        scripts = []
-        for root, dirs, files in os.walk(".", topdown=True):
-            dirs[:] = [d for d in dirs if d != "AI-Scripting-Tests"]  # Exclude the output subfolder
-            for file in files:
-                if file.endswith(".py") and file != sandbox_file:  # Exclude the current sandbox file
-                    scripts.append(os.path.join(root, file))
-        
-        # Loop until the time limit is reached
-        start_time = time.time()
-        while time.time() - start_time < MAX_TIME:
-            
-            # Choose a random script to read and learn from
-            script = random.choice(scripts)
-            
-            # Open the script file for reading
-            with open(script, "r") as script_file:
-                lines = script_file.readlines()
-                
-                # Loop through each line of the script and add it to the sandbox file
-                for line in lines:
-                    f.write(line)
-                    
-                    # Check if the time limit has been reached, and if so, save the sandbox file and start over
-                    if time.time() - start_time >= MAX_TIME:
-                        f.close()
-                        sandbox_num += 1
-                        break
-            
-            # Check if the time limit has been reached, and if so, save the sandbox file and start over
-            if time.time() - start_time >= MAX_TIME:
-                break
-    
-    # Increment the sandbox counter for the next iteration
-    sandbox_num += 1
+    filename = f"sandbox-{time.time()}.py"
+    output_path = os.path.join(output_dir, filename)
+    with open(output_path, "w") as file:
+        file.write(generated_script)
+
+    feedback = input("Did the script achieve the goal? (y/n)")
+    if feedback.lower() == "n":
+        continue
+
+    db_cursor.execute("INSERT INTO scripts (script) VALUES (?)", (generated_script,))
+    db_conn.commit()
+
+    more_feedback = input("How can the script be improved?")
+    with open(output_path, "a") as file:
+        file.write(f"\n\nFeedback:\n{more_feedback}\n")
+
+    print(f"Script saved to {output_path}\n")
