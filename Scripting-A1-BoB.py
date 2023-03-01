7993f3cf38948ebc2e749033e1736a1fae7aaf55
Shadowdrums <122622018+Shadowdrums@users.noreply.github.com>
2023-03-01 07:28:32 -0500

    Add files via upload

--- Scripting-A1-BoB.py
+++ Scripting-A1-BoB.py
@@ -3,54 +3,84 @@ import random
 import time
 import glob
 import sqlite3
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+import openai
+import re
+# Replace with your OpenAI API key
+openai.api_key = "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"
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-output_dir = os.path.join(dir_path, 'AI-Scripting-Tests')
+
+output_dir = os.getcwd()
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+try:
+    sid = SentimentIntensityAnalyzer()
+    sid.polarity_scores('test')
+except LookupError:
+    nltk.download('vader_lexicon')
+
 db_conn = sqlite3.connect("BoB-Database")
 db_cursor = db_conn.cursor()
-db_cursor.execute('''CREATE TABLE IF NOT EXISTS scripts
-                (id INTEGER PRIMARY KEY AUTOINCREMENT, script TEXT)''')
+db_cursor.execute('''CREATE TABLE IF NOT EXISTS scripts (id INTEGER PRIMARY KEY AUTOINCREMENT, script TEXT)''')
 db_conn.commit()
 
-MAX_SCRIPT_SIZE = 1000
-
 def generate_script():
-    scripts = [f for f in glob.glob(os.path.join(dir_path, '**/*.py'), recursive=True) if not f.endswith('sandbox.py')]
+    scripts = [f for f in glob.glob(os.path.join(output_dir, '**/*.py'), recursive=True) 
+               if not (f.endswith('.py') and 
+                       (re.match(r'^test-\d+\.py$', os.path.basename(f)) or 
+                        re.match(r'^Test-\d+\.py$', os.path.basename(f))))]
     script = ''
     for f in scripts:
         with open(f, 'r') as file:
             script += file.read()
 
     generated_script = ''
-    start_time = time.time()
-    while len(generated_script.split('\n')) < MAX_SCRIPT_SIZE and time.time() - start_time < 60:
+    while True:
         generated_script += random.choice(script.split('\n')) + '\n'
+        script_size = len(generated_script.split('\n'))
+        if script_size > 1:
+            script_file_name = os.path.join(output_dir, f'Test-{time.time()}.py')
+            with open(script_file_name, 'w') as f:
+                f.write(generated_script)
+            generated_script = generated_script.strip().replace('\n', '\n    ')
+            print(f"Generated script: \n\n    {generated_script}")
+            break
 
     return generated_script
 
-while True:
-    goal = input("Enter the goal of the script: ")
-    input(f"Press Enter to generate a new script with the goal: {goal}")
-    generated_script = generate_script()
-    print(f"Generated script:\n{generated_script}")
+def analyze_sentiment(script):
+    sia = SentimentIntensityAnalyzer()
+    return sia.polarity_scores(script)
 
-    filename = f"sandbox-{time.time()}.py"
-    output_path = os.path.join(output_dir, filename)
-    with open(output_path, "w") as file:
-        file.write(generated_script)
+def learning_with_nlp():
+    while True:
+        goal = input("What is your goal for the script? ")
+        print(f"Goal of the script: {goal}")
 
-    feedback = input("Did the script achieve the goal? (y/n)")
-    if feedback.lower() == "n":
-        continue
+        generated_script = ''
+        feedback = ''
+        while not feedback:
+            input("Press Enter to generate a new script")
+            prompt = f"Generate a script that {goal}"
+            completions = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.5)
+            generated_script = completions.choices[0].text
+            generated_script = re.sub('[^0-9a-zA-Z\n\.\,\(\)\[\]\{\}\_\+\-\=\*\&\^\%\$\#\@\!\~\`\?\>\<\:\;\'\"\|\\\]', '', generated_script)
+            generated_script = generated_script.strip()
+            sentiment_scores = analyze_sentiment(generated_script)
+            generated_script_formatted = generated_script.strip().replace('\n', '\n    ')
+            print(f"Generated script: \n\n    {generated_script_formatted}")
+            script_file_name = os.path.join(output_dir, f'Test-{time.time()}.py')
+            with open(script_file_name, 'w') as f:
+                f.write(generated_script)
+            feedback = input("Did the generated script achieve the goal? (y/n): ")
+            if feedback.lower() == 'n':
+                generated_script = ''
+                feedback = ''
 
-    db_cursor.execute("INSERT INTO scripts (script) VALUES (?)", (generated_script,))
-    db_conn.commit()
+        db_cursor.execute('''INSERT INTO scripts(script) VALUES (?)''', (generated_script,))
+        db_conn.commit()
 
-    more_feedback = input("How can the script be improved?")
-    with open(output_path, "a") as file:
-        file.write(f"\n\nFeedback:\n{more_feedback}\n")
-
-    print(f"Script saved to {output_path}\n")
+if __name__ == '__main__':
+    learning_with_nlp()
